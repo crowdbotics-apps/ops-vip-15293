6fc3d13e7ed58eaf2e06f686b8cb1ba6563e6dc3
Crowdbotics <team@crowdbotics.com>
2020-04-01 14:26:34 +0000

    Initial commit.

---
+++ backend/ops_vip_15293/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ops_vip_15293 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ops_vip_15293.settings')
+
+application = get_wsgi_application()
